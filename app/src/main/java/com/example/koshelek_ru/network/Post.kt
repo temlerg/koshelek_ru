26deb6f73664de5d402ad4255859e5022fab5e11
temlerg <56558240+temlerg@users.noreply.github.com>
2020-07-21 22:41:02 +0300

    Initial commit

---
+++ app/src/main/java/com/example/koshelek_ru/network/Post.kt
@@ -0,0 +1,7 @@
+package com.example.ttt.data.network
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+
+data class Post
---
+++ app/src/main/java/com/example/koshelek_ru/network/retrofit.kt
@@ -0,0 +1,31 @@
+package com.example.ttt.data.network
+
+import com.example.koshelek_ru.network.JSON
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object NetworkService {
+    private const val BASE_URL = "https://dog.ceo/api/breeds/list/all"
+
+    private val loggingInterceptor = run {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.apply {
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    fun getApiRepositories(): JSON {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+            .create(JSON::class.java)
+    }
+}
